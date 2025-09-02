10dfddd9f838a90026c15d3f905789b58da52a06
kealoliver-wq <keal.oliver@gmail.com>
2025-09-02 19:07:45 +0100

    Create deletion.js

---
+++ deletion.js
@@ -0,0 +1,19 @@
+export default function Deletion() {
+  return (
+    <main style={{ maxWidth: 720, margin: "40px auto", padding: 16, fontFamily: "system-ui, sans-serif" }}>
+      <h1>User Data Deletion</h1>
+      <p>
+        This WhatsApp Hub demo project is for internal testing only.
+      </p>
+      <p>
+        No personal data is permanently stored by this application.
+        However, if you would like to request deletion of any temporary
+        data associated with your WhatsApp messages, please contact the
+        project owner and all related data will be removed.
+      </p>
+      <p>
+        Contact: [add your email address or contact form link here]
+      </p>
+    </main>
+  );
+}
